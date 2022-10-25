72859c12e7494b413ed4765a953dd34cf887b623
annisatahira <annisatahira999@gmail.com>
2022-10-25 14:37:03 +0700

    chore: add project config

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../src/config/base";
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '@config/base'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
